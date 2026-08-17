8fb01c93cb4b321acd6f23d3013a2e6e8da8cee7
Sayan876 <120472214+Sayan876@users.noreply.github.com>
2026-08-17 13:08:25 +0530

    Update PortfolioBody.tsx

--- src/pages/PortfolioBody.tsx
+++ src/pages/PortfolioBody.tsx
@@ -5,7 +5,7 @@ import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import ResumeCTA from '@/components/ResumeCTA'
-
+import Architecture from '@/components/Architecture'
 import Skills from '@/components/Skills'
 
 
@@ -15,6 +15,7 @@ const PortfolioBody = () => {
       <Hero/>
       <About/>
       <Experience/>
+      <Architecture/>
       <Skills/>
       <Education/>
       <Projects/>
